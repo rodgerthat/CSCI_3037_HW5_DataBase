d4cc74f07d589ceefe844b1afc439205b298af34
joelrnorris <joel.r.norris@gmail.com>
2018-03-17 17:47:07 -0500

    import csv, create DB, populateDB,

--- hw5DataBase/Form1.Designer.cs
+++ hw5DataBase/Form1.Designer.cs
@@ -30,36 +30,87 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(21, 26);
+            this.button1.Location = new System.Drawing.Point(21, 75);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "Load DB";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(21, 67);
+            this.button2.Location = new System.Drawing.Point(21, 119);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
             this.button2.Text = "Clear DB";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(21, 33);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Create DB";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 205);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(119, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(639, 370);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(21, 160);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Add to DB";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(537, 390);
+            this.ClientSize = new System.Drawing.Size(770, 403);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "DataBase Shenanigans";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -67,6 +118,10 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- hw5DataBase/Form1.cs
+++ hw5DataBase/Form1.cs
@@ -1,20 +1,197 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace hw5DataBase
 {
     public partial class Form1 : Form
     {
+
+        public DataTable dataTable;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        // Load the DataBase
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            ImportCSV();
+            DataTableToDataBase(this.dataTable);
+            LoadDataGridViewWithDataTable(this.dataTable);
+
+            label1.Text = "DataBase has been Loaded!";
+        }
+
+        // Clear the DataBase
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            label1.Text = "DataBase has been Cleared!";
+        }
+
+        // Create the DataBase
+        private void button3_Click(object sender, EventArgs e)
+        {
+            CreateDataBase();
+
+        }
+
+        // Add Lines to DB
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CreateDataBase()
+        {
+
+            String str = "CREATE DATABASE studentGrade ON PRIMARY " +
+                "(NAME = studentGrade, " +
+                "FILENAME = 'studentGrade.mdf', " +
+                "SIZE = 2MB, MAXSIZE = 10MB, FILEGROWTH = 10%) " +
+                "LOG ON (NAME = studentGrade_Log, " +
+                "FILENAME = 'studentGrade_Log.ldf', " +
+                "SIZE = 1MB, " +
+                "MAXSIZE = 5MB, " +
+                "FILEGROWTH = 10%)";
+
+            SqlConnection myConn = new SqlConnection("Server=localhost;Integrated security=SSPI;database=master");
+
+            SqlCommand myCommand = new SqlCommand(str, myConn);
+
+            try
+            {
+                myConn.Open();
+                myCommand.ExecuteNonQuery();
+                label1.Text = "DataBase is Created Successfully";
+            }
+            catch (System.Exception ex)
+            {
+                label1.Text = ex.ToString();
+            }
+            finally
+            {
+                if (myConn.State == ConnectionState.Open)
+                {
+                    myConn.Close();
+                }
+            }
+        }
+
+        protected void ImportCSV()
+        {
+            // the csv file we're using
+            string csvFile = @"./csvDBData.txt";
+
+            // Create a DataTable.  
+            // Student Name,Class,Earned Hours,GPA,Course,CourseNumber,FinalGrade,TotalCSCICreditHrs,CSCI GPA
+            this.dataTable = new DataTable();
+            this.dataTable.Columns.AddRange(new DataColumn[9] {
+                new DataColumn("Student Name", typeof(string)),
+                new DataColumn("Class", typeof(string)),
+                new DataColumn("Earned Hours", typeof(int)),
+                new DataColumn("GPA",typeof(double)),
+                new DataColumn("Course",typeof(string)),
+                new DataColumn("CourseNumber",typeof(int)),
+                new DataColumn("FinalGrade",typeof(string)),
+                new DataColumn("TotalCSCICreditHrs",typeof(int)),
+                new DataColumn("CSCI GPA",typeof(double)),
+            });
+
+            // Read the contents of CSV file.  
+            string csvData = File.ReadAllText(csvFile);
+
+            // Execute a loop over the rows.  
+            foreach (string row in csvData.Split('\n'))
+            {
+                if (!string.IsNullOrEmpty(row))
+                {
+                    this.dataTable.Rows.Add();
+                    int i = 0;
+
+                    // Execute a loop over the columns.  
+                    foreach (string cell in row.Split(','))
+                    {
+                        this.dataTable.Rows[this.dataTable.Rows.Count - 1][i] = cell;
+                        i++;
+                    }
+                }
+            }
+
+        }
+
+        void LoadDataGridViewWithDataTable(DataTable dataTable)
+        {
+
+            // TODO : left off here, populating dataGridView with datatable
+            dataGridView1.DataSource = 
+        }
+
+        // take note of SqlBulkCopyOptions.KeepIdentity , you may or may not want to use this for your situation.  
+        private void DataTableToDataBase(DataTable dataTable)
+        {
+            string connectionString = GetConnectionString();
+            // string consString = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+
+                using (var bulkCopy = new SqlBulkCopy(sqlCon.ConnectionString, SqlBulkCopyOptions.KeepIdentity))
+                {
+
+                    // my DataTable column names match my SQL Column names, 
+                    // so I simply made this loop. However if your column names don't match, 
+                    // just pass in which datatable name matches the SQL column name in Column Mappings
+                    foreach (DataColumn col in dataTable.Columns)
+                    {
+                        bulkCopy.ColumnMappings.Add(col.ColumnName, col.ColumnName);
+                    }
+
+                    bulkCopy.BulkCopyTimeout = 600;
+                    bulkCopy.DestinationTableName = "studentGrade";
+                    bulkCopy.WriteToServer(dataTable);
+
+                }
+            }
+
+            // To avoid storing the connection string in your code, 
+            // you can retrieve it from a configuration file. 
+            string GetConnectionString()
+            {
+                return "Data Source=(local); " +
+                       " Integrated Security=true;" +
+                       "Initial Catalog=AdventureWorks;";
+            }
+
+            void DataBaseToDataTable()
+            {
+
+            }
+
+            void populateDataGridView()
+            {
+
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
     }
+
 }
